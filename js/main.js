c82f221506b73cc676cf2060362a1798a5d2388b
ElRafo92 <rafael.martinez@outlook.it>
2022-10-26 20:30:48 +0200

    aggiunto cambio immagini

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
-const immagini = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg", ];
+const immagini = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
+const box = document.querySelector(".box");
+
+for (i = 0; i < immagini.length; i++) {
+    let div = `<div class="item">
+                    <img src=${immagini[i]} alt="">
+                </div>`;
+
+    box.innerHTML += div;
+
+    const item = document.querySelector(".item");
+    item.classList.add("mostra");
+}
+
+let img = document.querySelectorAll('.item')
+console.log(img);
+
+const next = document.querySelector(".prossimo");
+let indexImg = 0;
+
+next.addEventListener('click', function() {
+    if (indexImg == img.length -1) {
+        img[indexImg].classList.remove('mostra');
+        indexImg += 1;
+        img[indexImg].classList.add('mostra');
+    }
+    else {
+        img[indexImg].classList.remove('mostra');
+        indexImg += 1;
+        img[indexImg].classList.add('mostra');
+
+    }
+})
+
+const prev = document.querySelector(".precedente");
+
+prev.addEventListener('click', function() {
+    if (indexImg == 0) {
+        img[indexImg].classList.remove('mostra');
+        indexImg -= 1;
+        img[indexImg].classList.add('mostra');
+    }
+    else {
+        img[indexImg].classList.remove('mostra');
+        indexImg -= 1;
+        img[indexImg].classList.add('mostra');
+
+    }
+})
+
